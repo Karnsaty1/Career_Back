1e2b92f0e0df030d24301acca4909ae28eae67df
Satyam <karnsaty69@gmail.com>
2024-11-24 12:56:53 +0545

    Updated

--- server.js
+++ server.js
@@ -67,4 +67,11 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something went wrong!');
 });
 
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
 module.exports = app;
