715b65c390ffe50cedefdd536df8f4fa32cc2906
Satyam <karnsaty69@gmail.com>
2025-01-25 11:29:52 +0545

    Updated

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(
 );
 
 const corsOptions = {
-  origin: [process.env.FRONTEND_URL,'http://localhost:3000'],
+  origin: [process.env.FRONTEND_URL,'http://localhost:3000','https://carrer-front-seven.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
